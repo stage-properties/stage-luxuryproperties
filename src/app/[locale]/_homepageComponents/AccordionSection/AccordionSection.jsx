605255ebc8a1fd1281fd267cddbcb3dced731496
stage-properties <it@stage-properties.com>
2025-09-07 12:34:24 +0300

    fix css

--- src/app/[locale]/_homepageComponents/AccordionSection/AccordionSection.jsx
+++ src/app/[locale]/_homepageComponents/AccordionSection/AccordionSection.jsx
@@ -5,13 +5,17 @@ import ImageAccordion from "../../_components/ImageAccordion/ImageAccordion";
 
 export default function AccordionSection({ title1, title2, items }) {
   return (
-    <section className="wrapper accordionSection">
-      <div className="accordionHeader">
-        {title1 && <h2 className="title1">{title1}</h2>}
-        {title2 && <h3 className="title2">{title2}</h3>}
-      </div>
+    <section className="accordionSection">
+      <div className="accordionBackground" />
+      <div className="accordionOverlay" />
+      <div className="wrapper accordionContent">
+        <div className="accordionHeader">
+          {title1 && <h2 className="title1">{title1}</h2>}
+          {title2 && <h3 className="title2">{title2}</h3>}
+        </div>
 
-      <ImageAccordion items={items} />
+        <ImageAccordion items={items} />
+      </div>
     </section>
   );
 }
