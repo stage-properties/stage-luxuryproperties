03914227fccb1740287eca1a455c1530fd5ccb8c
stage-properties <it@stage-properties.com>
2025-09-07 11:33:16 +0300

    fixing CSS

--- src/app/[locale]/_homepageComponents/CommunitySection/CommunitySection.jsx
+++ src/app/[locale]/_homepageComponents/CommunitySection/CommunitySection.jsx
@@ -2,21 +2,18 @@ import CommuntiySlider from "./CommuntiySlider";
 import { getFeaturedCities } from "../service";
 import { getTranslations } from "next-intl/server";
 
-import Link from "next/link";
-
-const CommunitySection = async ({ locale, community_subheading }) => {
+const CommunitySection = async ({ locale }) => {
   const t = await getTranslations({ locale, namespace: "common" });
   const featuredCityData = await getFeaturedCities(`page=1&locale=${locale}`);
 
   return (
     <div className="communitySection">
       <div className="wrapper">
-        <h2 className="mainHeading gradientText">{t("dubai_s_most")}</h2>
-        <h2 className="mainHeading gradientText">{t("coveted_enclaves")}</h2>
-        <h3 className="mainHeading gradientText marginTop">
-          {t(
-            "explore_the_neighborhoods_where_luxury_is_not_just_a_standard_but_a_lifestyle"
-          )}
+        <h2 className="title">{t("dubai_s_most")}</h2>
+        <h2 className="subtitle">{t("coveted_enclaves")}</h2>
+        <h3 className="text">
+          Explore the neighborhoods where luxury is not just a<br /> standard,
+          but a lifestyle.
         </h3>
         <CommuntiySlider featuredCityData={featuredCityData?.data} />
       </div>
