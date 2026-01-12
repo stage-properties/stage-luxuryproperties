c7c95ab8dc3263ac8c517962e9cc35ddcf9c617d
stage-properties <it@stage-properties.com>
2026-01-12 12:49:22 +0200

    fixing home page mobile

--- src/app/[locale]/_homepageComponents/BlogsSection/BlogSection.jsx
+++ src/app/[locale]/_homepageComponents/BlogsSection/BlogSection.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { getTranslations } from "next-intl/server";
 import { fetchBlogs } from "@/app/[locale]/_commonService/network";
 import BlogCarousel from "./components/BlogCarousel";
